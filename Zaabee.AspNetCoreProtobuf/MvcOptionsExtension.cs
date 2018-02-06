866b7acd822867f26a6f95aac1c30909e4284036
杜小非 <aeondxf@live.com>
2018-02-06 16:49:12 +0800

    Complete

---
+++ Zaabee.AspNetCoreProtobuf/MvcOptionsExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Zaabee.AspNetCoreProtobuf
+{
+    public static class MvcOptionsExtension
+    {
+        public static void AddProtobufSupport(this MvcOptions options)
+        {
+            options.InputFormatters.Add(new ProtobufInputFormatter());
+            options.OutputFormatters.Add(new ProtobufOutputFormatter());
+            options.FormatterMappings.SetMediaTypeMappingForFormat("protobuf",
+                MediaTypeHeaderValue.Parse("application/x-protobuf"));
+        }
+    }
+}
