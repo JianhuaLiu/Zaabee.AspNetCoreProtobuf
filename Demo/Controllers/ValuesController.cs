2ade0c93772827bd81fa4c058a3dc92f2db792ce
杜小非 <aeondxf@live.com>
2018-02-08 15:54:06 +0800

    Update the Test.

--- Demo/Controllers/ValuesController.cs
+++ Demo/Controllers/ValuesController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Controllers
@@ -8,66 +7,10 @@ namespace Demo.Controllers
     public class ValuesController : Controller
     {
         // GET api/values
-        [HttpGet]
-        public IEnumerable<TestDto> Get()
+        [HttpPost]
+        public IEnumerable<TestDto> Post([FromBody]IEnumerable<TestDto> dtos)
         {
-            return new List<TestDto>
-            {
-                new TestDto
-                {
-                    Id = Guid.NewGuid(),
-                    Tag = long.MaxValue,
-                    CreateTime = DateTime.Now,
-                    Name = "0",
-                    Enum = TestEnum.Apple,
-                    Kids = new List<TestDto>
-                    {
-                        new TestDto
-                        {
-                            Id = Guid.NewGuid(),
-                            Tag = long.MaxValue - 1,
-                            CreateTime = DateTime.Now,
-                            Name = "00",
-                            Enum = TestEnum.Banana
-                        },
-                        new TestDto
-                        {
-                            Id = Guid.NewGuid(),
-                            Tag = long.MaxValue - 2,
-                            CreateTime = DateTime.Now,
-                            Name = "01",
-                            Enum = TestEnum.Pear
-                        }
-                    }
-                },
-                new TestDto
-                {
-                    Id = Guid.NewGuid(),
-                    Tag = long.MaxValue - 3,
-                    CreateTime = DateTime.Now,
-                    Name = "1",
-                    Enum = TestEnum.Apple,
-                    Kids = new List<TestDto>
-                    {
-                        new TestDto
-                        {
-                            Id = Guid.NewGuid(),
-                            Tag = long.MaxValue - 4,
-                            CreateTime = DateTime.Now,
-                            Name = "10",
-                            Enum = TestEnum.Banana
-                        },
-                        new TestDto
-                        {
-                            Id = Guid.NewGuid(),
-                            Tag = long.MaxValue - 5,
-                            CreateTime = DateTime.Now,
-                            Name = "11",
-                            Enum = TestEnum.Pear
-                        }
-                    }
-                }
-            };
+            return dtos;
         }
     }
 }
--- UnitTest/AspNetCoreProtobufTest.cs
+++ UnitTest/AspNetCoreProtobufTest.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using Demo;
 using Xunit;
 
@@ -24,31 +25,104 @@ namespace UnitTest
         }
 
         [Fact]
-        public async Task Test()
+        public void Test()
         {
-            // Act
+            // HTTP Post with Protobuf Response Body
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
-            var response = await _client.GetAsync("/api/values");
-            response.EnsureSuccessStatusCode();
 
-            var result = ProtoBuf.Serializer.Deserialize<List<TestDto>>(await response.Content.ReadAsStreamAsync());
+            var dtos = GetDtos();
+            var stream = new MemoryStream();
+            ProtoBuf.Serializer.Serialize(stream, dtos);
 
-            // Assert
-            Assert.Equal("application/x-protobuf", response.Content.Headers.ContentType.MediaType);
+            HttpContent data = new StreamContent(stream);
+
+            // HTTP POST with Protobuf Request Body
+            var responseForPost = _client.PostAsync("api/Values", data);
+
+            var result = ProtoBuf.Serializer.Deserialize<List<TestDto>>(
+                responseForPost.Result.Content.ReadAsStreamAsync().Result);
+
+            Assert.True(CompareDtos(dtos,result));
+        }
+
+        private static bool CompareDtos(List<TestDto> lstOne, List<TestDto> lstTwo)
+        {
+            lstOne = lstOne ?? new List<TestDto>();
+            lstTwo = lstTwo ?? new List<TestDto>();
             
-            Assert.Equal(long.MaxValue, result[0].Tag);
-            Assert.Equal(long.MaxValue-1, result[0].Kids[0].Tag);
-            Assert.Equal(long.MaxValue-2, result[0].Kids[1].Tag);
-            Assert.Equal(long.MaxValue-3, result[1].Tag);
-            Assert.Equal(long.MaxValue-4, result[1].Kids[0].Tag);
-            Assert.Equal(long.MaxValue-5, result[1].Kids[1].Tag);
+            if (lstOne.Count != lstTwo.Count) return false;
             
-            Assert.Equal("0", result[0].Name);
-            Assert.Equal("00", result[0].Kids[0].Name);
-            Assert.Equal("01", result[0].Kids[1].Name);
-            Assert.Equal("1", result[1].Name);
-            Assert.Equal("10", result[1].Kids[0].Name);
-            Assert.Equal("11", result[1].Kids[1].Name);
+            for (var i = 0; i < lstOne.Count; i++)
+            {
+                var dtoOne = lstOne[i];
+                var dtoTwo = lstTwo[i];
+                if (dtoOne.Id != dtoTwo.Id || dtoOne.CreateTime != dtoTwo.CreateTime || dtoOne.Enum != dtoTwo.Enum ||
+                    dtoOne.Name != dtoTwo.Name || dtoOne.Tag != dtoTwo.Tag || CompareDtos(dtoOne.Kids, dtoTwo.Kids))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static List<TestDto> GetDtos()
+        {
+            return new List<TestDto>
+            {
+                new TestDto
+                {
+                    Id = Guid.NewGuid(),
+                    Tag = long.MaxValue,
+                    CreateTime = DateTime.Now,
+                    Name = "0",
+                    Enum = TestEnum.Apple,
+                    Kids = new List<TestDto>
+                    {
+                        new TestDto
+                        {
+                            Id = Guid.NewGuid(),
+                            Tag = long.MaxValue - 1,
+                            CreateTime = DateTime.Now,
+                            Name = "00",
+                            Enum = TestEnum.Banana
+                        },
+                        new TestDto
+                        {
+                            Id = Guid.NewGuid(),
+                            Tag = long.MaxValue - 2,
+                            CreateTime = DateTime.Now,
+                            Name = "01",
+                            Enum = TestEnum.Pear
+                        }
+                    }
+                },
+                new TestDto
+                {
+                    Id = Guid.NewGuid(),
+                    Tag = long.MaxValue - 3,
+                    CreateTime = DateTime.Now,
+                    Name = "1",
+                    Enum = TestEnum.Apple,
+                    Kids = new List<TestDto>
+                    {
+                        new TestDto
+                        {
+                            Id = Guid.NewGuid(),
+                            Tag = long.MaxValue - 4,
+                            CreateTime = DateTime.Now,
+                            Name = "10",
+                            Enum = TestEnum.Banana
+                        },
+                        new TestDto
+                        {
+                            Id = Guid.NewGuid(),
+                            Tag = long.MaxValue - 5,
+                            CreateTime = DateTime.Now,
+                            Name = "11",
+                            Enum = TestEnum.Pear
+                        }
+                    }
+                }
+            };
         }
     }
 }
